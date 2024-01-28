35d7cb8e6a681e2150b52ca2105000f88087d1f3
s26766-pj <115408528+s26766-pj@users.noreply.github.com>
2024-01-28 18:06:37 +0100

    kolokwium

---
+++ MPR/s26766-bank/src/main/java/com/example/s26766bank/S26766BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s26766bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S26766BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S26766BankApplication.class, args);
+	}
+
+}
